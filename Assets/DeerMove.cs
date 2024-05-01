8c00eee0eca77a99c075b0a4aa4d9993ff9d8fb9
Bethchen <138103628+Bethchen123@users.noreply.github.com>
2024-05-01 22:34:59 +0800

    buildd
    長身體、撞牆會死

--- Assets/DeerMove.cs
+++ Assets/DeerMove.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class DeerMove : MonoBehaviour
 {
     Vector3 direction;
+    public Transform bodyPrefab;
+    public List<Transform> bodies = new List<Transform>();
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 0.7f;
+        bodies.Add(transform);
     }
 
     // Update is called once per frame
@@ -34,6 +38,33 @@ public class DeerMove : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for (int i = bodies.Count - 1; i > 0; i--)
+        {
+            bodies[i].position = bodies[i - 1].position;
+        }
         transform.Translate(direction);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Gift"))
+        {
+            //bodies.Add(Instantiate(bodyPrefab));
+            bodies.Add(Instantiate(bodyPrefab, transform.position, Quaternion.identity));
+        }
+        //if (collision.CompareTag("obstacle"))
+        //{
+        //    ResetStage();
+        //}
+    }
+    private void ResetStage()
+    {
+        transform.position = Vector3.zero;
+        direction = Vector3.zero;
+
+        for(int i = 1; i < bodies.Count; i++)
+        {
+            Destroy(bodies[i].gameObject);
+        }
+        bodies.Add(transform);
     }
 }
